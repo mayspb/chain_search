ad1c40fbe6dd6a62b7790fb2e1ebe433a80803ff
Michael.Yatsenko <michael.yatsenko@gmail.com>
2017-04-22 17:13:19 +0300

    Initial commit

---
+++ src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  std::string input;
+  std::getline(std::cin, input);
+  std::cout << input << std::endl;
+  return 0;
+}
