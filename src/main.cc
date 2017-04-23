f4048e787dfd933dec56abe25dca4fba2c94e1c0
Michael.Yatsenko <michael.yatsenko@gmail.com>
2017-04-23 11:07:41 +0300

    done

--- src/main.cc
+++ src/main.cc
@@ -5,35 +5,45 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  std::string input, output;
+  // Input data and first mask check
+  std::string input;
   std::getline(std::cin, input);
   std::regex regex("\\s*\\+?\\d+(\\s+(([+-]?\\d+(\\.\\d*([eE][+-]?\\d*)?)?)|([+-]?\\d*[eE]\\d+))?)*(\\\\n)?");
   if (std::regex_match(input, regex)) {
-    //std::cout << "input valid" << std::endl;
-    output = std::regex_replace(input, std::regex("\\s{2,}"), " ");
-    output = std::regex_replace(output, std::regex("\\\\n"), "");
-    //output = std::regex_replace(output, std::regex("^\\s"), "");
-    //output = std::regex_replace(output, std::regex("\\s$"), "");
+    input = std::regex_replace(input, std::regex("\\\\n"), "");
   } else {
     std::cout << "[error]";
     return 0;
   }
+
+  // Prepare data for processing and check task conditions
   std::vector<std::string> input_values;
-  std::stringstream s(output);
+  std::stringstream s(input);
   std::string value;
   while (s >> value)
     input_values.push_back(value);
-  value = std::regex_replace(input_values[0], std::regex("\\+"), "");
-  int length = std::stoi(value);
+  int length;
+  try {
+    length = std::stoi(input_values[0]);
+  } catch (...) {
+    std::cout << "[error]";
+    return 0;
+  }
   if ((input_values.size() - 1) < length || length < 2) {
     std::cout << "[error]";
     return 0;
   }
   std::vector<long double> source_chain;
   for (int i = 1; i < input_values.size(); i++) {
-    source_chain.push_back(std::stold(input_values[i]));
+    try {
+      source_chain.push_back(std::stold(input_values[i]));
+    } catch (...) {
+      std::cout << "[error]";
+      return 0;
+    }
   }
 
+  // Process: searching the chain
   long double prev = source_chain[0];
   bool place_flag = false;
   int place, place_prev;
@@ -57,12 +67,12 @@ int main(int argc, char *argv[]) {
     }
     prev = source_chain[i];
   }
-
-  //std::cout << output << std::endl;
   if (subchain_length == source_chain.size() || subchain_length > subchain_length_prev) {
     subchain_length_prev = subchain_length;
     place_prev = place;
   }
+
+  // Final output
   if (subchain_length_prev != 1) {
     std::cout << subchain_length_prev << " " << place_prev;
   } else {
